994a8cf697404a4b08dec2fcda4c7b326206f5fa
Bao Tran <baog1tran@gmail.com>
2026-05-19 13:02:43 -0400

    update the login page

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,1 +1,10 @@
+function doLogin() {
+    let email = document.getElementById("login_email").value;
+    let password = document.getElementById("login_password").value;
 
+    // This prints to your browser console in real time!
+    console.log("Submit button clicked!");
+    console.log("Attempting login with email:", email);
+    
+    // Your AJAX code continues here... ????
+}
